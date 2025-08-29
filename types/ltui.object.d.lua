87c03ccaa4bad9367506db7294ad8afa2f7accda
Chromatischer <chromatischer@hildania.de>
2025-08-29 18:06:49 +0200

    Deleted types/ directory for it is no longer needed

--- types/ltui.object.d.lua
+++
@@ -1,9 +0,0 @@
----Base object system
---- @meta
-
---- @class ltui.object
---- Base object class with field initialization system.
---- Already has comprehensive type annotations in source file.
-local object = {}
-
-return object
